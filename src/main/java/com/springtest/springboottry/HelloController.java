afbc127f8e5994b82aca1ee6f61e60775d89985f
xinwei_ucc <xinwei_xcc@sina.com>
2023-11-16 19:37:33 +0900

    build test

--- src/main/java/com/springtest/springboottry/HelloController.java
+++ src/main/java/com/springtest/springboottry/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot trying!";
+		return "Greetings from Spring Boot trying!---build Test";
 	}
 
 }
